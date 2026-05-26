e76bb6d05e36b35fa3a4febcfad4cde99fc2f4b0
zenil <savanizenil2611@gmail.com>
2026-05-26 22:47:28 +0530

    littel changees

--- src/utils/response.js
+++ src/utils/response.js
@@ -32,3 +32,14 @@ module.exports = {
     successResponse,
     errorResponse
 };
+
+// class ApiError extends Error {
+//     constructor(statusCode, message, errors = null) {
+//         super(message)
+
+//         this.statusCode = statusCode
+//         this.errors = errors
+//     }
+// }
+
+// export default ApiError
